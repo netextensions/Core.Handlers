0c9d463df76da9174f26025fa6dd44224cff76fa
Csaba Salat <csalat@globalblue.com>
2020-04-12 15:06:20 +0200

    init

---
+++ src/Core.Handlers/RequestHandlerBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace NetExtensions
+{
+    public abstract class RequestHandlerBase<TContext, TRequest, TResponse> : Repository<TContext>, IRequestHandler<TRequest, TResponse> where TRequest : IRequest<TResponse> where TContext : DbContext, new()
+    {
+        protected IMapper Mapper;
+
+        protected RequestHandlerBase(ILogger<RequestHandlerBase<TContext, TRequest, TResponse>> logger, DbContextOptions<TContext> options, IMapper mapper) : base(logger, options)
+        {
+            Mapper = mapper;
+        }
+
+        public abstract Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken);
+    }
+}
