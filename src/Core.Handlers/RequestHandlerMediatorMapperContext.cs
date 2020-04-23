b6478083cb8f0ee15da1dc1b67acd6919983b584
Csaba Salat <csalat@globalblue.com>
2020-04-23 11:56:59 +0200

    add mapper

---
+++ src/Core.Handlers/RequestHandlerMediatorMapperContext.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace NetExtensions
+{
+    public abstract class RequestHandlerMediatorMapperContext<TContext, TRequest, TResponse> : RequestHandlerContext<TContext, TRequest, TResponse> where TRequest : IRequest<TResponse> where TContext : DbContext
+    {
+        protected readonly IMediator Mediator;
+        protected readonly IMapper Mapper;
+
+        protected RequestHandlerMediatorMapperContext(ILogger<RequestHandlerContext<TContext, TRequest, TResponse>> logger, DbContextOptions<TContext> options, IMediator mediator, IMapper mapper) : base(logger, options)
+        {
+            Mediator = mediator;
+            Mapper = mapper;
+        }
+    }
+}
